d7eff39b5b5ccbd028d6a71aa6847056db813cbb
Amene <layla_1380@yahoo.com>
2024-08-20 20:08:04 -0500

    practicing decorators

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+   def __init__(self, brand,size ):
+      self.brand= brand
+      self.size= size
+
+   @property
+   def brand(self):
+        return self._brand
+   @brand.setter
+   def brand(self, brand):
+      self._brand = brand
+
+   @property
+   def size(self):
+       return self._size
+   @size.setter
+   def size(self, size):
+       if isinstance(size,int):
+           self._size = size
+       else:
+          print('size must be an integer')   
+   def cobble(self):
+        """Repairs the shoe and sets its condition to 'New'."""
+        print("Your shoe is as good as new!")
+        self.condition = "New"  # Set the condition to "New"
