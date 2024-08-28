4c762cd25b6801a03b9d247173944cf7678e50ac
Amene <layla_1380@yahoo.com>
2024-08-28 07:20:15 -0500

    new submission

--- lib/book.py
+++ lib/book.py
@@ -1,32 +1,51 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
+# class Book:
+#     def __init__(self, title, page_count):
+#         self.title = title
+#         self.page_count = page_count
+
+#     @property
+#     def title(self):
+#         return self._title
+#     @title.setter
+#     def title(self, title):
+#         self._title = title
+
+#     @property
+#     def page_count(self):
+#         return self._page_count
+    
+#     @page_count.setter
+#     def page_count(self, page_count):
+#         if isinstance(page_count, int):
+#             self._page_count = page_count
+#         else:
+#             print('page_count must be an integer')
+
+#     def  turn_page(self):
+#         print('Flipping the page...wow, you read fast!') 
+
+
+       
 class Book:
-    def __init__(self, title, page_count):
-        self.title = title
-        self.page_count = page_count
-
-    @property
+    def __init__(self,title, page_count):
+       self.title = title
+       self.page_count = page_count 
+    @property 
     def title(self):
         return self._title
     @title.setter
     def title(self, title):
         self._title = title
-
     @property
     def page_count(self):
-        return self._page_count
-    
+        return self._page_count 
     @page_count.setter
     def page_count(self, page_count):
         if isinstance(page_count, int):
-            self._page_count = page_count
+            self._page_count = page_count 
         else:
             print('page_count must be an integer')
-
-    def  turn_page(self):
-        print('Flipping the page...wow, you read fast!') 
-
-
-       
-    
-        
+    def turn_page(self):
+        print('Flipping the page...wow, you read fast!')
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,27 +1,62 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
+
+# class Shoe:
+#    def __init__(self, brand,size ):
+#       self.brand= brand
+#       self.size= size
+
+#    @property
+#    def brand(self):
+#         return self._brand
+#    @brand.setter
+#    def brand(self, brand):
+#       self._brand = brand
+
+#    @property
+#    def size(self):
+#        return self._size
+#    @size.setter
+#    def size(self, size):
+#        if isinstance(size,int):
+#            self._size = size
+#        else:
+#           print('size must be an integer')   
+#    def cobble(self):
+#         """Repairs the shoe and sets its condition to 'New'."""
+#         print("Your shoe is as good as new!")
+#         self.condition = "New"  # Set the condition to "New"
 
 class Shoe:
-   def __init__(self, brand,size ):
-      self.brand= brand
-      self.size= size
-
-   @property
-   def brand(self):
+    def __init__(self, brand, size, condition = 'New'):
+        self.brand = brand
+        self.size = size
+        self.condition = condition
+    @property
+    def brand(self):
         return self._brand
-   @brand.setter
-   def brand(self, brand):
-      self._brand = brand
+    @brand.setter
+    def brand(self, brand):
+        self._brand = brand
 
-   @property
-   def size(self):
-       return self._size
-   @size.setter
-   def size(self, size):
-       if isinstance(size,int):
-           self._size = size
-       else:
-          print('size must be an integer')   
-   def cobble(self):
-        """Repairs the shoe and sets its condition to 'New'."""
-        print("Your shoe is as good as new!")
-        self.condition = "New"  # Set the condition to "New"
+    @property
+    def size(self):
+        return self._size
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+           print('size must be an integer')
+
+    @property
+    def condition(self):
+        return self._condition
+    @condition.setter
+    def condition(self, condition):
+        self._condition = condition
+
+    def cobble(self):
+        print('Your shoe is as good as new!')
+        self.condition = 'New'
+
+            
